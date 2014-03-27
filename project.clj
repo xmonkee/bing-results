2390a3525c8d18dceaa45638e5d69fa284e7ea11
Mayank Mandava <mayankmandava@gmail.com>
2014-03-27 12:04:25 +0530

    Changed Names

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bing-results "0.1.0-SNAPSHOT"
   :description "Download number of bing results for all queries supplied in a file on the commandline"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/xmonkee/bing-results"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
